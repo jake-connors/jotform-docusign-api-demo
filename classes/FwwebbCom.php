8eedd2cd4eff33dca7697783f8205d2eb34b9c7c
jake-connors <41030082+jake-connors@users.noreply.github.com>
2025-10-14 10:01:38 -0400

    Add fallback GET coordinates call to FwwebbCom.php class

--- classes/FwwebbCom.php
+++ classes/FwwebbCom.php
@@ -1,6 +1,7 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/Globals.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/Erp.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/Curl.php";
 
 class FwwebbCom {
     /** 
@@ -87,6 +88,23 @@ class FwwebbCom {
             $result_state = $user_coords_result["zipcode"]["state"];
         }
 
+        if ($result_lat === "" || $result_lng === "" || $result_state === "") {
+            // fwwebb endpoint is down sometimes, so fallback to zippopotam.us (public API)
+            $zippopotam_result = self::_getCoordsFromZippopotam($zip);
+
+            if (isset($zippopotam_result["latitude"])) {
+                $result_lat = $zippopotam_result["latitude"];
+            }
+
+            if (isset($zippopotam_result["longitude"])) {
+                $result_lng = $zippopotam_result["longitude"];
+            }
+
+            if (isset($zippopotam_result["state"])) {
+                $result_state = $zippopotam_result["state"];
+            }
+        }
+
         $ret = [
             "latitude" => $result_lat,
             "longitude" => $result_lng,
@@ -94,6 +112,27 @@ class FwwebbCom {
         ];
 
         return $ret;
+    }
+
+    /**
+     * Gets coordinates from zippopotam.us (public API)
+    */
+    private static function _getCoordsFromZippopotam($zip) {
+        $url = "https://api.zippopotam.us/us/" . urlencode($zip);
+
+        $rawResp = \Curl::doGet($url);
+
+        $respData = json_decode($rawResp, true);
+
+        $lat = $lng = $state = "";
+        if (isset($respData["places"][0])) {
+            $place = $respData["places"][0];
+            $lat = $place["latitude"] ?? "";
+            $lng = $place["longitude"] ?? "";
+            $state = $place["state abbreviation"] ?? "";
+        }
+
+        return ["latitude" => $lat, "longitude" => $lng, "state" => $state];
     }
 }
 ?>
